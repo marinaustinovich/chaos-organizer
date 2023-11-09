fbbe18fb5dffd3e37ecc989516a1f474385d5cb6
marinaustinovich <115401561+marinaustinovich@users.noreply.github.com>
2023-11-09 23:24:43 +0300

    fix errors into moveFile

--- back/actions/moveFile.js
+++ back/actions/moveFile.js
@@ -2,12 +2,14 @@
 const fs = require('fs');
 const fsp = fs.promises;
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 async function moveFile(file, type) {
-    const uploadDir = path.join(__dirname, 'public', type);
+    const serverDir = path.join(__dirname, '..'); 
+    const uploadDir = path.join(serverDir, 'public', type);
     await fsp.mkdir(uploadDir, { recursive: true });
 
-    const newFilename = `${Date.now()}-${file.originalFilename}`;
+    const newFilename = `${uuidv4()}-${file.originalFilename}`;
     const savePath = path.join(uploadDir, newFilename);
 
     try {
@@ -22,33 +24,6 @@ async function moveFile(file, type) {
     }
 
     return path.join(type, newFilename).replace(/\\/g, '/');
-}
-
-async function copyFile(src, dest) {
-    return new Promise((resolve, reject) => {
-        const readStream = fs.createReadStream(src);
-        const writeStream = fs.createWriteStream(dest);
-
-        readStream.on('data', (chunk) => console.log(`Read ${chunk.length} bytes`));
-        writeStream.on('drain', () => console.log('Write stream drained'));
-
-        readStream.on('end', () => {
-            writeStream.end(); 
-        });
-        readStream.on('error', (error) => {
-            console.error('Read stream error:', error);
-            reject(error);
-        });
-        writeStream.on('error', (error) => {
-            console.error('Write stream error:', error);
-            reject(error);
-        });
-        writeStream.on('close', () => {
-            resolve();
-        });
-
-        readStream.pipe(writeStream);
-    });
 }
 
 module.exports = moveFile;

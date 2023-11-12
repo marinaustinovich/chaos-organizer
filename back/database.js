0f3f6074c739618dcb2e9b28e5a9b913240c5019
marinaustinovich <115401561+marinaustinovich@users.noreply.github.com>
2023-11-12 11:00:31 +0300

    update config

--- back/database.js
+++ back/database.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
+
+console.log('DB Username:', process.env.DB_USERNAME);
+console.log('DB Password:', process.env.DB_PASSWORD);
+console.log('DB Name:', process.env.DB_NAME);
+console.log('DB Host:', process.env.DB_HOST);
 const { Sequelize } = require('sequelize');
+
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+const host = process.env.DB_HOST;
 
-const sequelize = new Sequelize('organizer', username, password, {
-  host: 'localhost',
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
   dialect: 'mysql',
 });
 
